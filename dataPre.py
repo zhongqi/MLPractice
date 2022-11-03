5851a9a64acef6c59e01836a415ae4bcffdd143f
Lixiang1613 <lixiang.zhou@student.manchester.ac.uk>
2022-11-03 19:45:30 +0800

    This ammendmant is mainly on get_data() of dataPre.py. 03/Nov/22 19:45:00:00

--- dataPre.py
+++ dataPre.py
@@ -5,21 +5,37 @@
 from param import tushare_token, loc_db
 import json
 import sqlite3
+import pandas as pd
+import time
+import csv
+
 
 # get data from web and cache locally
 def get_data(compCode):
     import tushare
     tushare.set_token(tushare_token)
 
-    conn = sqlite3.connect(loc_db)
+    conn = sqlite3.connect(loc_db)  # Lixiang's db location: 'D:/PyProjects/TimeSeriesAnaPrj_database/CHN_stock_data.db'
+
     c = conn.cursor()
-    
+
+    pro = tushare.pro_api()
+    t = time.strftime('%Y%m%d', time.localtime())
+    # df = pro.daily(trade_date=t)  # 获取所有上市公司当前日期的全部历史
+    df = pro.query('daily', ts_code=compCode, trade_date=t)  # 获取制定公司当前日期的所有历史
+    sql = """
+            create table if not exists AShareDaily(ts_code,trade_date,`open`,high,low,`close`,pre_close,change,pct_chg,vol,amount);
+        """
+    c.execute(sql)
+    df.to_sql('AShareDaily', conn, if_exists='append', index=False)
+    conn.commit()
+
+    c.close()
+    conn.close()
+
 
 # provide a data service interface
-def service_data(comCode):
+def service_data(compCode):
     result = {}
     result[compCode] = []
     return json.dumps(result)
-
-
-
