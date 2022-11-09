f191093d9914d40f50100915d91ca200efd46ac1
Zhongqi Lu <zhongqi@outlook.com>
2022-11-09 18:20:58 +0800

    hotfix featureEng.py

--- featureEng.py
+++ featureEng.py
@@ -5,29 +5,29 @@ from param import loc_db
 class featureEng:
     def __init__():
         self.loc_database = loc_db
-        self.data = None
+        self.data = self._get_data()
         self.num_features = 1000 #predefined, fill 0 by default
         self.features = []
 
     # load data in memory
-    def get_data():
+    def _get_data():
         pass
 
     # feature engineering method 1
-    def feature_eng1():
+    def _feature_eng1():
         feature = {}
         return feature
 
     # feature engineering method 2
-    def feature_eng2():
+    def _feature_eng2():
         feature = {}
         return feature
 
     def feature_eng_all():
-        f1 = self.feature_eng1()
+        f1 = self._feature_eng1()
         self.features.append(f1)
 
-        f2 = self.feature_eng2()
+        f2 = self._feature_eng2()
         self.features.append(f2)
 
     # done.
