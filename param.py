0e4a392e007208789d136024f8f2cea4230654d9
Dalia <644703781@qq.com>
2023-07-13 23:50:51 +0800

    在dataPre中加入one-hot编码函数

--- param.py
+++ param.py
@@ -1,2 +1,6 @@
 tushare_token = "" #register at https://tushare.pro/register?reg=431908
 loc_db = "local.db"
+
+datapath = './data/样本.xlsx'
+num_of_component = 3  # 样本数据中功能组件的类别数量,目前只有三种，”单元算法“，”材料模型“，”求解流程“
+target_component = "材料模型"  # 对目标组件生成数据集
